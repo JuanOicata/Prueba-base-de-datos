53a8cd7ca548f054d0be04728d4b156e1210fba6
Juan Oicata <jeotoicata@gmail.com>
2024-09-19 19:24:13 -0500

    (Nueva): configuracion del proyecto con Springboot

---
+++ src/main/java/co/ucentral/Ejemplobasededatos/EjemplobasededatosApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.Ejemplobasededatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplobasededatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplobasededatosApplication.class, args);
+	}
+
+}
